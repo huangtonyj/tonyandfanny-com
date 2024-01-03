d649dd7fe737e716726e99140115e5eb809f21dc
tonyjhuang <huangtonyj91@gmail.com>
2024-01-03 01:54:32 -0800

    Add Rsvp section

--- app/wedding/page.tsx
+++ app/wedding/page.tsx
@@ -22,7 +22,7 @@ export default function App() {
       <Venue />
       {/* <OurStory /> */}
       <Schedule />
-      {/* <Rsvp /> */}
+      <Rsvp />
       {/* <TravelIdeas /> */}
       {/* <Footer /> */}
     </div>
