28bf5ebc33db08b0525db994c1b2c81fcdf225b8
tonyjhuang <huangtonyj91@gmail.com>
2024-01-03 00:55:42 -0800

    clean Schedule.tsx

--- app/lib/Schedule.tsx
+++ app/lib/Schedule.tsx
@@ -5,8 +5,8 @@ export default function Schedule(): ReactNode {
     <section
       id="schedule"
       className="py-12 md:py-18 px-12
-                w-screen h-96
-                flex gap-12 lg:gap-24 flex-col md:flex-row justify-evenly items-center
+                w-screen min-h-96
+                flex gap-12 flex-col md:flex-row justify-evenly items-center
                 bg-gray-200"
     >
       <div>
@@ -14,14 +14,19 @@ export default function Schedule(): ReactNode {
           Schedule
         </h1>
       </div>
-      <div className="flex flex-col md:flex-row gap-12">
-        <div>
-          <p className="text-2xl text-center font-light text-black">05:00 PM</p>
+      <div className="flex flex-col gap-12">
+        <div className="flex flex-col lg:flex-row lg:gap-12">
+          <p className="text-xl text-center font-light text-black">05:00 PM</p>
           <p className="text-xl text-center font-light text-black">Ceremony</p>
         </div>
 
-        <div>
-          <p className="text-2xl text-center font-light text-black">06:30 PM</p>
+        <div className="flex flex-col lg:flex-row lg:gap-12">
+          <p className="text-xl text-center font-light text-black">05:30 PM</p>
+          <p className="text-xl text-center font-light text-black">Cocktail</p>
+        </div>
+
+        <div className="flex flex-col lg:flex-row lg:gap-12">
+          <p className="text-xl text-center font-light text-black">06:30 PM</p>
           <p className="text-xl text-center font-light text-black">Reception</p>
         </div>
       </div>
